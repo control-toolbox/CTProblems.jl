fa395a6bfcad889ece14579815474f44321c07e4
Cots <boris.wembe@irit.fr>
2023-03-20 15:31:40 +0100

    revise

--- test/test_problem_manual.jl
+++ test/test_problem_manual.jl
@@ -1,6 +1,16 @@
 #
+using Revise
+try
+    revise(CTProblems)
+catch
+end
+
+using CTProblems
+
 prob = Problem(:integrator, :dim2)
 
 display(prob.model)
 
 plot(prob.solution)
+
+println(prob.message)
