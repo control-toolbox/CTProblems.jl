bf324f5231c2b2d34bdebbe134b9ffcb51c10252
Olivier Cots <olivier.cots@enseeiht.fr>
2023-03-24 23:00:48 +0100

    deb unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,21 @@
 using CTProblems
 using Test
+using MINPACK
+using CTFlows
+using MLStyle
+using CTBase
+using LinearAlgebra
+include("utils.jl")
 
 @testset verbose = true showtiming = true "CTProblems" begin
-    for name in (
-        "problem", 
+    for name ∈ (
+        :simple_integrator,
+        :double_integrator,
         )
-        @testset "$name" begin
-            include("test_$name.jl")
+        @testset "$(name)" begin
+            test_name = Symbol(:test_, name)
+            include("$(test_name).jl")
+            @eval $test_name()
         end
     end
 end
