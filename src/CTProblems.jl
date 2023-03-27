1b6e54860d00766d92ef7e6b12539ec8f2794257
Baptiste Combelles <baptiste.combelles@proton.me>
2023-03-27 17:26:25 +0200

    fix CTProblem due to bad merge

--- src/CTProblems.jl
+++ src/CTProblems.jl
@@ -10,15 +10,23 @@ include("problem.jl")
 #
 include("problems/simple_exponential_energy.jl")
 include("problems/double_integrator_energy.jl")
+include("problems/simple_turnpike.jl")
+include("problems/simple_integrator_abs.jl")
+include("problems/simple_integrator_energy_free.jl")
+include("problems/simple_integrator_squaresum_free.jl")
+include("problems/simple_integrator_time_free.jl")
 include("problems/goddard.jl")
 
 # list of examples
-examples = ()
-examples = add(examples, (:integrator, :dim2, :energy))
-examples = add(examples, (:goddard, :state_constraint))
-examples = add(examples, (:integrator, :dim1, :energy))
-
-Problems() = examples
+problems = ()
+problems = add(problems, (:integrator, :dim2, :energy))
+problems = add(problems, (:goddard, :state_constraint))
+problems = add(problems, (:exponential, :dim1, :energy))
+problems = add(problems, (:integrator, :dim1, :time, :free, :constraint))
+problems = add(problems, (:turnpike, :dim1))
+problems = add(problems, (:integrator, :dim1, :squaresum, :free))
+problems = add(problems, (:integrator, :dim1, :absolute, :constraint))
+problems = add(problems, (:dummy, )) # to test exception not implemented
 
 #
 export Problems, Problem
--- test/test_problem_manual.jl
+++ test/test_problem_manual.jl
@@ -7,7 +7,7 @@ end
 
 using CTProblems
 
-prob = Problem(:exponential, :dim1, :energy)
+prob = Problem(:turnpike, :dim1)
 
 display(prob.model)
 
