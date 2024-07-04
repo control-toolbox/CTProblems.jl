7fb10e739a16331cd15ad590f82a0156020295d5
Olivier Cots <olivier.cots@irit.fr>
2024-07-04 15:34:36 +0200

    foo

--- docs/make.jl
+++ docs/make.jl
@@ -10,14 +10,6 @@ makedocs(
     modules = [CTProblems],
     sitename = "CTProblems.jl",
     format = Documenter.HTML(prettyurls = false),
-    strict = [
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-    ],
     pages = [
         "Introduction" => "index.md",
         "Problems" => ["descriptions-list.md", "problems-list.md"],
